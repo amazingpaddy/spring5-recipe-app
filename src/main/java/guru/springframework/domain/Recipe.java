416fcb1b683e51538dd14d89a5bbd005bbef80a3
Paddy <padmanabhan5789@gmail.com>
2022-01-26 18:23:54 -0500

    Created Required Entities.

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,118 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private long id;
+
+  private String description;
+  private int prepTime;
+  private int cookTime;
+  private int servings;
+  private String source;
+  private String url;
+  private String directions;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+  private Set<Ingredient> ingredients;
+
+  @Lob
+  private byte[] image;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  private Notes notes;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public int getPrepTime() {
+    return prepTime;
+  }
+
+  public void setPrepTime(int prepTime) {
+    this.prepTime = prepTime;
+  }
+
+  public int getCookTime() {
+    return cookTime;
+  }
+
+  public void setCookTime(int cookTime) {
+    this.cookTime = cookTime;
+  }
+
+  public int getServings() {
+    return servings;
+  }
+
+  public void setServings(int servings) {
+    this.servings = servings;
+  }
+
+  public String getSource() {
+    return source;
+  }
+
+  public void setSource(String source) {
+    this.source = source;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getDirections() {
+    return directions;
+  }
+
+  public void setDirections(String directions) {
+    this.directions = directions;
+  }
+
+  public byte[] getImage() {
+    return image;
+  }
+
+  public void setImage(byte[] image) {
+    this.image = image;
+  }
+
+  public Notes getNotes() {
+    return notes;
+  }
+
+  public void setNotes(Notes notes) {
+    this.notes = notes;
+  }
+
+  public Set<Ingredient> getIngredients() {
+    return ingredients;
+  }
+
+  public void setIngredients(Set<Ingredient> ingredients) {
+    this.ingredients = ingredients;
+  }
+}
